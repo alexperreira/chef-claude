a4e907ca4f1c29461079072e29501644daf64476
alexperreira <alexanderperreira@gmail.com>
2024-11-26 09:35:22 -0700

    Minor CSS changes on Main component, added Inter font

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,31 @@
 export default function Main () {
+    const ingredients=["Chicken", "Soy", "Garlic"]
+
+    const ingredientsListItems=ingredients.map( ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ) )
+
+    function handleSubmit ( event ) {
+        event.preventDefault()
+        const formData=new FormData( event.currentTarget )
+        const newIngredient=formData.get( 'ingredient' )
+        ingredients.push( newIngredient )
+    }
+
     return (
         <main>
-            <form className='add-ingredient-form' action="">
+            <form onSubmit={handleSubmit} className='add-ingredient-form' action="">
                 <input
-                    type="text"
+                    type='text'
                     aria-label='Add ingredient'
                     placeholder='e.g. oregano'
+                    name='ingredient'
                 />
                 <button type='submit'>Add ingredient</button>
             </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
         </main>
     )
 }
